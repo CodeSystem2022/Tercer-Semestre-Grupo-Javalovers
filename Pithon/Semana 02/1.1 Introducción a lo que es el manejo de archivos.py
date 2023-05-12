197ab35b193b085015abc992b165b695dd8109b1
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-12 15:39:07 -0300

    Update 1.1 Introducción a lo que es el manejo de archivos.py

--- Pithon/Semana 02/1.1 Introducción a lo que es el manejo de archivos.py
+++ Pithon/Semana 02/1.1 Introducción a lo que es el manejo de archivos.py
@@ -1,1 +1,9 @@
+#Declaramos una variable
 
+try:
+    archivo = open('prueba.txt', 'w') #Entre parentecis indicamos nuestro metodo Open va a buscar el archivo caso contrario que si el archivo no existe lo va a clear
+                                  #La w de Write que significa escribir
+except Exception as e:
+    print(e)
+finally: #siempre se ejecuta
+    archivo.close() #Con esto se debe cerrar el archivo
