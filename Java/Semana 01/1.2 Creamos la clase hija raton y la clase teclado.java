42d923dda2c476bbc8d15d5f09299998b350a3fb
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-11 22:28:18 -0300

    Update 1.2 Creamos la clase hija raton y la clase teclado.java

--- Java/Semana 01/1.2 Creamos la clase hija raton y la clase teclado.java
+++ Java/Semana 01/1.2 Creamos la clase hija raton y la clase teclado.java
@@ -1,1 +1,38 @@
+package ar.com.system2023.mundopc;
 
+public class Teclado extends DispositivoEntrada{
+     private final int idTeclado;
+    private static int contadorTeclados;
+    
+    
+    public Teclado(String tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado{" + "idTeclado=" + idTeclado +", "+super.toString()+'}';
+    }
+}
+
+
+
+
+
+package ar.com.system2023.mundopc;
+
+public class Raton extends DispositivoEntrada{
+    private final int idRaton;
+    private static int contadorRatones;
+    
+    
+    public Raton(String tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRatones;
+    }
+
+    @Override
+    public String toString() {
+        return "Raton{" + "idRaton=" + idRaton +", "+super.toString()+'}';
+    }
