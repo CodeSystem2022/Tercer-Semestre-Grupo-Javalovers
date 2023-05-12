0a6b1ed50743272961d4637780270f6700545da3
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-12 15:48:40 -0300

    Update 1.1 Introducción a lo que es el manejo de archivos.py

--- Pithon/Semana 02/1.1 Introducción a lo que es el manejo de archivos.py
+++ Pithon/Semana 02/1.1 Introducción a lo que es el manejo de archivos.py
@@ -3,7 +3,9 @@
 try:
     archivo = open('prueba.txt', 'w') #Entre parentecis indicamos nuestro metodo Open va a buscar el archivo caso contrario que si el archivo no existe lo va a clear
                                   #La w de Write que significa escribir
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n') #El metodo Write es para lograr escribir dentro del archivo "prueba.txt"
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: #siempre se ejecuta
-    archivo.close() #Con esto se debe cerrar el archivo
+    archivo.close() #Con esto se debe cerrar el archivo  
