50803ec48a8367d0cc4f4c9d986f0026528f3831
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-11 22:39:59 -0300

    Update 1.2 Arreglos.js

--- JavaScript/Semana 01/1.2 Arreglos.js
+++ JavaScript/Semana 01/1.2 Arreglos.js
@@ -1,1 +1,15 @@
+//While
+let contador = 0;
+while (contador < 3){
+    console.log(contador);  0, 1, 2
+    contador++;
+}
+consola.log("Fin del ciclo while");   Fin del ciclo while
 
+//Do while
+let conteo = 0;
+do{
+    console.log(conteo); 0, 1, 2
+    conteo++;
+}while(conteo < 3);
+console.log("Fin del ciclo do while"); Fin del ciclo do while 
