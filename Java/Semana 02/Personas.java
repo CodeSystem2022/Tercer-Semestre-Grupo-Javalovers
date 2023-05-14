257359a6e24bb158574880ded773e5383058f788
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-05-14 00:14:02 -0300

    Update Personas.java

--- Java/Semana 02/Personas.java
+++ Java/Semana 02/Personas.java
@@ -3,3 +3,10 @@ package  domain;
 public class Persona {
     private final int idPersona;
     private static int contadorPersonas;
+static{ //Bloque de inicializacion estatico
+        System.out.println("Ejecucion bloque estatico");
+        ++Persona.contadorPersonas;
+      //idPersona=10;No es estatico un atributo, por esto tenemos un error
+       
+    }
+    
