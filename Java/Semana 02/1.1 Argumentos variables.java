aa72d4643a65b9ce8427429e717100d836ed0846
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-05-13 23:35:30 -0300

    Update 1.1 Argumentos variables.java

--- Java/Semana 02/1.1 Argumentos variables.java
+++ Java/Semana 02/1.1 Argumentos variables.java
@@ -11,3 +11,11 @@ public class TestArgumentoVariables {
         imprimirNumeros(numeros);
     }
     
+    private static void imprimirNumeros(int ...numeros) {
+        for(int i = 0; i < numeros.length; i++)  {
+            System.out.println("Elementos: "+numeros[i]);
+        
+    }
+    }
+}
+
