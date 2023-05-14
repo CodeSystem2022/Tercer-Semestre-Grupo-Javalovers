36a03452d572b95644f31b65a09255bd817acc2c
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-13 23:02:17 -0300

    Update 1.1 Argumentos variables.java

--- Java/Semana 02/1.1 Argumentos variables.java
+++ Java/Semana 02/1.1 Argumentos variables.java
@@ -10,9 +10,4 @@ public class TestArgumentoVariables {
         System.out.println("Nombre: "+nombre+," Apellido: "+apellido);
         imprimirNumeros(numeros);
     }
-    private static void imprimirNumeros( int ...numeros){
-        for (int i = 0, i< numeros.length ; i++) {
-            System.out.println("Elementos:" +numeros[i]);
-        }
-    }
-}
+    
