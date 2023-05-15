39d02975a9426914c65e97d6d57235ba62c6b2ce
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-05-15 18:33:08 -0300

    Update  Arreglos en JavaScript.js

--- JavaScript/Semana 02/ Arreglos en JavaScript.js
+++ JavaScript/Semana 02/ Arreglos en JavaScript.js
@@ -2,3 +2,6 @@
 //let autos = new Array('Ferrai', 'Renault', 'BMW'); esta es la sintaxis vieja
 const autos = ['Ferrai', 'Renault', 'BMW'];
 console.log(autos);
+//Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
