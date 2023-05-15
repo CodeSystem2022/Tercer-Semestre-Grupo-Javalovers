edf64d404ff1bd5ad0639a3774acbe53448e16d2
Carrizo Andres <93688247+andrescarrizo1@users.noreply.github.com>
2023-05-15 16:28:53 -0300

    Create 1.6 Etiquetas Labels.js

---
+++ JavaScript/Semana 01/1.6 Etiquetas Labels.js
@@ -0,0 +1,9 @@
+//la palabra continue y etiquetas Labels
+inicio:
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !==0){
+       break inicio; //it a la siguente iteracion
+       }
+       console.log(contando); 0
+ }
+ console.log("Termina el ciclo"); termina el ciclo
