12a156f9c12cb64fbebcad5cc54f340c4bed1978
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-22 12:22:10 -0300

    Update 3.6 Modificador de acceso private.java

--- Java/Semana 03/3.6 Modificador de acceso private.java
+++ Java/Semana 03/3.6 Modificador de acceso private.java
@@ -1,1 +1,16 @@
+//Del Archivo:TestDefaulf
+package paquete1;
 
+public class TestDefaulf {
+    public static void main (String [] args){
+      ClaseHija2 claseH2 = new ClaseHija2();
+      claseH2.atributoDefault = "Cambio desde la prueba";
+      System.out.println("claseH2 atributo default = " + claseH2.atributoDefault);
+      
+      Clase4 clase4 = new Clase4("Publico");
+      System.out.println(clase4.getAtributoPrivate());
+      clase4.setAtributoPrivate("Cambio");
+      System.out.println("Clase4= " + clase4.getatributoPrivate());
+    }
+    
+}
