9fd413789f24380a9375a7801f788152c1a98dc1
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-05-21 22:21:37 -0300

    Update 3.2 Retorno palabra.js

--- JavaScript/Semana 03/3.2 Retorno palabra.js
+++ JavaScript/Semana 03/3.2 Retorno palabra.js
@@ -1,1 +1,13 @@
+function miFuncion(a, b){
+    //console.log("Sumamos: "+(a + b));
+    return  a + b;
+
+}
+
+//Llamando la funcion 
+miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
+
 
