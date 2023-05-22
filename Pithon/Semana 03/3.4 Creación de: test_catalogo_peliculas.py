0a3edd4d8bd184d598bd3b0f86812c28b78a4d41
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-21 23:45:54 -0300

    Update 3.4 Creación de: test_catalogo_peliculas.py

--- Pithon/Semana 03/3.4 Creación de: test_catalogo_peliculas.py
+++ Pithon/Semana 03/3.4 Creación de: test_catalogo_peliculas.py
@@ -1,1 +1,15 @@
-
+opcion = None
+while opcion != 4:
+    try:
+        print('Opciones')
+        print('1. Agregar Pelicula')
+        print('2. Lista las peliculas')
+        print('3. Eliminar catalogo de pelicula')
+        print('4. Salir')
+        opcion = int(input('Digite una opción de menú (1-4):'))
+    except Exception as e:
+        print(f'Ocurrio un eror de tipo:{e}')
+        opcion = None
+    else:
+        print('Salimos del programa')
+        
