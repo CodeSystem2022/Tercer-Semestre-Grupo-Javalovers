b3dafcd47db6edc9ea71af4cceba60c287af66fc
Javier <112019412+javierr66@users.noreply.github.com>
2023-05-23 01:58:25 -0300

    Update 1.3-1.4 arreglos javascript.js

--- JavaScript/Semana 02/1.3-1.4 arreglos javascript.js
+++ JavaScript/Semana 02/1.3-1.4 arreglos javascript.js
@@ -6,16 +6,23 @@ console.log(autos[2]);
 for(let i = 0; i < autos.lenght; i++){
     console.log(i+' : '+autos[i])
 }
+//Modificar los elementos del arreglo 
 autos[1] = 'Volvo';
 console.log(autos[1]); Volvo
 
-autos.push('Audi');
+//Agregamos nuevos valores al arreglo 
+autos.push('Audi');//Agregamos el elemento al final del arreglo 
 console.log(autos);
 
 autos[autos.length] = 'porshe'
 console.log(autos);
 
+//Tercera forma, tener cuidado al utilizarlo (podemos dejar un espacio vacio o reemplazar un dato)
 autos[6] = 'Renault'
 console.log(autos)
 
+//Preguntar  si es un arrray(arreglo para los panas)
+console.log(Array.isArray(autos));//Devuelve un booleano
+
+//Otra manera por dudas de que te muestren un algoritmo
 consaole.log(autos instanceof Array);
