24944149f0bd15f8786009934367316f1541a543
Arroganzt <112448203+Arroganzt@users.noreply.github.com>
2023-05-26 23:53:49 -0300

    Add files via upload

---
+++ Java/Semana 03/3.2 Autoboxing y Unboxing Parte 1.java
@@ -0,0 +1,17 @@
+
+package test;
+
+public class Testautoboxingunboxing {
+    public static void main(String[] args) { 
+        //Clases envolventes a wrapper
+        /*
+            clases envolventes de tipos primitivos
+            int= la clase envolvente es integer
+         */
+       
+        int enteroPrim = 10;
+        System.out.println("enteroprim = " + enteroPrim);
+        Integer entero = 10;
+        System.out.println("entero = " + entero);
+    }         
+}
