db11efb51aae1791e356499a7e93701225a236bc
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-30 22:57:35 -0300

    Update 1.2 Ejercicio: Sobreescritura de métodos Overriding Parte 1 y 2.java

--- Java/semana 04/1.2 Ejercicio: Sobreescritura de métodos Overriding Parte 1 y 2.java
+++ Java/semana 04/1.2 Ejercicio: Sobreescritura de métodos Overriding Parte 1 y 2.java
@@ -1,1 +1,11 @@
 //Archivo: Gerente.java
+package domain;
+
+public class Gerente extends Empleado{
+    private String-departamento;
+    
+    public Gerente(String nombre, double sueldo, String departamento){
+        super(nombre, sueldo);
+        this.departamento = departamento;
+    } 
+}
