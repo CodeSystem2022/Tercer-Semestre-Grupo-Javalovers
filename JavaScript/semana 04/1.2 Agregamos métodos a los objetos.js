e5275e054e4160740920833c8ad0170a238f24d5
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-05-31 16:36:39 -0300

    Update 1.2 Agregamos métodos a los objetos.js

--- JavaScript/semana 04/1.2 Agregamos métodos a los objetos.js
+++ JavaScript/semana 04/1.2 Agregamos métodos a los objetos.js
@@ -1,1 +1,19 @@
+//Objeto 
+let persona = {
+    nombre: "Carlos",
+    apellido: "Gil",
+    email: "cgil@gmail.com",
+    edad: 30,
+    nombreCompleto: function(){ //Metodo o funcion en JavaScript
+        return this.nombre+" "+this.apellido;
+       
+}
 
+
+}
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+console.log(persona);
+console.log(persona.nombreCompleto());
