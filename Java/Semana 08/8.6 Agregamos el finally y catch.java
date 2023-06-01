3f9b403d4af1c99f922a12c3284e669fbae53e65
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-05-31 21:34:11 -0300

    Update 8.6 Agregamos el finally y catch.java

--- Java/Semana 08/8.6 Agregamos el finally y catch.java
+++ Java/Semana 08/8.6 Agregamos el finally y catch.java
@@ -1,1 +1,13 @@
+package test;
 
+import static aritmetica.Aritmetica.division;
+
+public class TestExcepciones{
+    public static void main(String [] args) {
+        int resultado = 0;
+        try{
+            resultado = division( 10, 0);
+        }
+    }
+    
+}
