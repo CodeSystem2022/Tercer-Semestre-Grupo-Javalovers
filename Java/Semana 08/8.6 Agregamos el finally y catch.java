b20d453695efa43b293e9f0aa90eea5f937d58ae
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-06-01 00:53:03 -0300

    Update 8.6 Agregamos el finally y catch.java

--- Java/Semana 08/8.6 Agregamos el finally y catch.java
+++ Java/Semana 08/8.6 Agregamos el finally y catch.java
@@ -7,6 +7,22 @@ public class TestExcepciones{
         int resultado = 0;
         try{
             resultado = division( 10, 0);
+          
+        } catch (OperacionExcepciones e) {
+             System.out.println("Ocurrio un error de tipo OperacionExcepciones");
+             System.out.println(e.getMessage());
+        } catch (Excepcion e) {
+             System.out.println("Ocurrio un Error");
+             e.printStackTrace(System.out) ; //se conoce como la pila de excepciones
+             System.out.println(e.getMessage()); 
+        }
+         finally{
+             System.out.println("Se reviso la division entra cero");
+        }
+         System.out.println("La variable de resultado tiene como valor: "+resultado) ;
+                                
+                                
+                                 
         }
     }
     
