91b409d58bf5f45b05559c5b68eeb8036cb7b251
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-06-01 01:11:17 -0300

    Update 8.2 Atributo estatico.js

--- JavaScript/Semana 08/8.2 Atributo estatico.js
+++ JavaScript/Semana 08/8.2 Atributo estatico.js
@@ -1,1 +1,6 @@
-
+modificaciones en este cogido (ATRIBUTO ESTATICO)
+class Persona{ //Clase padre
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
