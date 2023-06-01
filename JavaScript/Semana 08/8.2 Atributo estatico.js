e0467810a08008fa95d58d4f1aa246749a6a500b
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-06-01 01:17:55 -0300

    Update 8.2 Atributo estatico.js

--- JavaScript/Semana 08/8.2 Atributo estatico.js
+++ JavaScript/Semana 08/8.2 Atributo estatico.js
@@ -1,6 +1,11 @@
 modificaciones en este cogido (ATRIBUTO ESTATICO)
 class Persona{ //Clase padre
+    static contadorObjetosPersona = 0;
+    
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        Persona.contadorObjetosPersona++;
+        console.log("Se incrementa el contador: "+Persona.contadorObjetosPersona);
+        
     }
