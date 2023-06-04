0fb0ab03090590d1a41c669cf653623aaf5aabe0
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-06-04 11:32:34 -0300

    Update 5.3 Función fechall en psycopg2 Parte 1 y 2.py

--- Pithon/Semana 05/5.3 Función fechall en psycopg2 Parte 1 y 2.py
+++ Pithon/Semana 05/5.3 Función fechall en psycopg2 Parte 1 y 2.py
@@ -1,1 +1,12 @@
+import psycopg2  #Esto es para poder conectarnos a 
 
+conexion = psycopg2.connect(
+    user= 'postgres',
+    password= 'admin',
+    host= '127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
