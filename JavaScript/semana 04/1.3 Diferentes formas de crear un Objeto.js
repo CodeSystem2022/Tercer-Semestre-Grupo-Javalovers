da58f3e710a3a05473c4fd885b9667e647e9672a
Carrizo Andres <93688247+andrescarrizo1@users.noreply.github.com>
2023-06-06 17:50:48 -0300

    Create 1.3 Diferentes formas de crear un Objeto.js

---
+++ JavaScript/semana 04/1.3 Diferentes formas de crear un Objeto.js
@@ -0,0 +1,5 @@
+Let persona2 = new Object(); //debe crear un nuevo objeto en memoria
+persona2.nombre = ´Juan';
+persona2.direccion = 'Salada 14'
+persona2.telefono = '549261746446';
+console.log(persona2.telefono); '549261746446
