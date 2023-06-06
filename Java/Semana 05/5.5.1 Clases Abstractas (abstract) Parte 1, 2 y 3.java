8ea148305fa94fd128f49cefae80ac3e30330c3c
Carrizo Andres <93688247+andrescarrizo1@users.noreply.github.com>
2023-06-06 19:20:38 -0300

    Create 5.5.1 Clases Abstractas (abstract) Parte 1, 2 y 3.java

---
+++ Java/Semana 05/5.5.1 Clases Abstractas (abstract) Parte 1, 2 y 3.java
@@ -0,0 +1,9 @@
+package test;
+
+import domian.*;
+
+public class TestAbstracatas {
+  public static void main (Sting[] args) {
+    FiguraGeometrica figura = new Rectangulo("Rectangulo");
+    figura.dibujar();
+  }
