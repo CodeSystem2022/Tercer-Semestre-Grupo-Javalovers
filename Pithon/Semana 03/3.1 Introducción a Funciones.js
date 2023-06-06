2314cf1f0e0f7e0077f904efb2cd4456f24c2d99
Javier <112019412+javierr66@users.noreply.github.com>
2023-06-06 04:03:22 -0300

    3.1 Introducción a Funciones.js

---
+++ Pithon/Semana 03/3.1 Introducción a Funciones.js
@@ -0,0 +1,11 @@
+//llamar a la funcion antes de estar definirla, esto se llama hoisting 
+miFuncion(8, 2);
+
+function miFuncion(a, b){//escritura camelcase
+    console.log('Sumamos: '+ (a+b));
+}
+//Llamando  la funcion
+miFuncion(5, 4);
+
+//video 2 palabra return
+//miFuncion2(1, 4);
