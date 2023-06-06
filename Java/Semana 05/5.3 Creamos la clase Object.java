0d7317c42294d1f5c00f4db36dcfdd8abf4e8042
Carrizo Andres <93688247+andrescarrizo1@users.noreply.github.com>
2023-06-06 19:14:43 -0300

    Create 5.3 Creamos la clase Object.java

---
+++ Java/Semana 05/5.3 Creamos la clase Object.java
@@ -0,0 +1,16 @@
+public void main (Sting[] args){
+   Empleado empleado1 = new Empleado ("Juan",5000);
+   Empleado empleado2 = new Empleado ("Juan",5000);
+  
+   if(empleado1 == empleado2){
+     System.out.println("Tienen la misma referencia en memoria");
+   }
+  else{
+    System.out.println("Tienen distinta referencia en memoria");
+  }
+   if(empleado1.equals(empleado2)){
+     System.out.println("Los objetos son iugales en cpntenido");
+   }
+  else{
+    System.out.println("Los objetos son distintos en contenido");
+  }
