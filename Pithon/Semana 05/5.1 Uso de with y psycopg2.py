b55c77b12d441681870f421021ae57f6ae1ad560
Carrizo Andres <93688247+andrescarrizo1@users.noreply.github.com>
2023-06-06 23:20:33 -0300

    Create 5.1 Uso de with y psycopg2.py

---
+++ Pithon/Semana 05/5.1 Uso de with y psycopg2.py
@@ -0,0 +1,21 @@
+import psycopg2 #Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect(
+  user='postgres',
+  password='admin'
+  host='127.0.0.1',
+  port='5432',
+  database='test_bd'
+)
+try:
+ with conexion:
+  with conexion.cursor() as cursor:
+      sentencia = 'SELECT * FROM persona'
+      cursor.execute(sentencia) #De esta manera ejecutamos la sentencia
+      registros = cursor.fetchall() #Recuperamos todos los registros que seran una lista
+      print(registros)
+except Exception as e:
+  print(f'Ocurrio un error: {e}')
+  finally:
+  
+  cursor.close()
