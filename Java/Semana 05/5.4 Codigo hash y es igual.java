a072a9d2319297fb655db92b7d5dca515dafdc29
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-06-17 22:31:54 -0300

    Update 5.4 Codigo hash y es igual.java

--- Java/Semana 05/5.4 Codigo hash y es igual.java
+++ Java/Semana 05/5.4 Codigo hash y es igual.java
@@ -1,1 +1,10 @@
+//El METODO EQUALS compara el contenido de dos objetos (en el caso siguiente el objeto es nombre)
+//podemos comparar el atributo de nombre en este caso es posible por que el objeto nombre es de tipo string
+if objects.equals(this.nombre, other.nombre));
 
+//  Compara el  objeto para saber si son iguales
+if(empleado1.equals(empleado2)){
+     System.out.println("Los objetos son iguales en contenido");
+   }
+  else{
+    System.out.println("Los objetos son distintos en contenido");
