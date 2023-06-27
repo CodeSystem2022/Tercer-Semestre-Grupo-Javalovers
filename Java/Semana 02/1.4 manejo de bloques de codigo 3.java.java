8b81100cc60fc086b7862bdae2ac182ab0f0171d
Arroganzt <112448203+Arroganzt@users.noreply.github.com>
2023-06-26 22:46:19 -0300

    Add files via upload

---
+++ Java/Semana 02/1.4 manejo de bloques de codigo 3.java.java
@@ -0,0 +1,13 @@
+
+package test;
+
+import domain.Persona;
+
+public class testbloqueinicializacion {
+    public static void main(String[] args)(
+    persona personal= new Persona();
+    system.out.println("personal = " + personal);
+    persona persona2 = new Persona();
+    System.out.println(¨persona2 = ¨ +persona2);
+    )
+}
