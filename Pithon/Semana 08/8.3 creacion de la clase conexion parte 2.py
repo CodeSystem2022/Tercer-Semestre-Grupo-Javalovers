81c2622e0320bfb04b7f50666f8ec36e75a4150a
Javier <112019412+javierr66@users.noreply.github.com>
2023-06-29 05:30:23 -0300

    8.3 creacion de la clase conexion parte 2..2py
    video 5 de la clase 8

--- Pithon/Semana 08/8.3 creacion de la clase conexion parte 2.py
+++ Pithon/Semana 08/8.3 creacion de la clase conexion parte 2.py
@@ -11,19 +11,35 @@ class conexion:
     _HOST = '127.0.0.1'
     _conexion = None
     _cursor = None
+    
+    @classmethod
+    def obtenerConexion(cls):
+        is cls._conexion in None:
+          try:
+              cls._conexion = bd.connect(host=cls._HOST,
+                                         user=cls._USERNAME,
+                                         password=cls._PASSWORD,
+                                         port=cls._DATABASE)
+              log.debug(f'Conexcion Exitosa: {cls._conexion}')
+              return cls._conexion
+          except Exception as e:
+            log.error(f'Ocurrio un error: {e}')
+            sys.exit()
+        else:
+            return cls._conexion
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'Se abrio corectamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrio un error: {e}')
+                sys.exit()
+            else:
+                return cls._cursor
 
-  @classmethod
-  def obtenerConexion(cls):
-    is cls._conexion in None:
-      try:
-          cls._conexion = bd.connect(host=cls._HOST,
-                                     user=cls._USERNAME,
-                                     password=cls._PASSWORD,
-                                     port=cls._DATABASE)
-        log.debug(f'Conexcion Exitosa: {cls._conexion}')
-        return cls._conexion
-      except Exception as e:
-      log.error(f'Ocurrio un error: {e}')
-      sys.exit()
-    else:
-        return cls._conexion
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
