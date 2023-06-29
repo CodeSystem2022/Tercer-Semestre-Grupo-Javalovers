951c3ebc6a12ef1dfa06496569dbf19cb04f4c30
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-06-29 16:12:13 -0300

    Update 10.2 Creación de la clase Persona ->Parte 1, 2 y 3.java

--- Java/semana 10/10.2 Creación de la clase Persona ->Parte 1, 2 y 3.java
+++ Java/semana 10/10.2 Creación de la clase Persona ->Parte 1, 2 y 3.java
@@ -28,4 +28,45 @@ public class Persona{
         return nombre;
     }
 
+
+    #Parte 2
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getTel(){
+        return tel;
+    }
+
+    public void setTel(String tel){
+        this.tel = tel;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        return "Persona{" +
+                "id=" + id +
+                ", nombre ='" + nombre + '\' '+
+                ", tel = ' " + tel + '\'' + 
+                ", email = '" + email + '\'' + 
+                '}';
+    }
+    public static void main (String [] arg){
+        Persona persona1 = new Persona("Juan Perez", "261707071", "jperez@gmail.com");
+        System.out.println(persona1);
+    }
+    
+
+
+}
+
     
