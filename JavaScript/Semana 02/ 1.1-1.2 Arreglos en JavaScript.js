a329b11415d0a5206d9b8d8bb77a6bfd91668a54
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-06-30 17:33:23 -0300

    Update  1.1-1.2 Arreglos en JavaScript.js

--- JavaScript/Semana 02/ 1.1-1.2 Arreglos en JavaScript.js
+++ JavaScript/Semana 02/ 1.1-1.2 Arreglos en JavaScript.js
@@ -5,4 +5,7 @@ console.log(autos);
 //Recorremos los elementos de un arreglo
 console.log(autos[0]);
 console.log(autos[2]);
-
+for (let i = 0; i < autos.length; i++){
+    console.log(autos [i]);
+    //otra forma:console.log(i+' : '+autos[i]);
+}
