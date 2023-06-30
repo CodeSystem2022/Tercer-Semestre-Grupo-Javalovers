8a0a889f8322e21431a76511ac97a5929aa82f17
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-06-30 20:21:23 -0300

    Create 3.5 Tipos de datos en una función.js

---
+++ JavaScript/Semana 03/3.5 Tipos de datos en una función.js
@@ -0,0 +1,10 @@
+console.log(typeof miFuncion); function
+function miFuncionDos(a, b){
+    console.log(arguments.length)
+}
+
+miFuncionDos(5, 7, 3, 6);
+
+//toString
+var miFuncionTexto = miFuncionDos.toString();
+console.log(miFuncionTexto); ... ionDos(a, b) { var $_$c = $_$wf(1); $_$w(1, 12, $_
