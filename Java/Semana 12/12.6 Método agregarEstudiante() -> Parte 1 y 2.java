81a5d93955e5a85ee16d2f97a7c94ac4342722a7
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-06-30 18:56:19 -0300

    Update 12.6 Método agregarEstudiante() -> Parte 1 y 2.java

--- Java/Semana 12/12.6 Método agregarEstudiante() -> Parte 1 y 2.java
+++ Java/Semana 12/12.6 Método agregarEstudiante() -> Parte 1 y 2.java
@@ -1,1 +1,16 @@
+// Metodo agregar un nuevo estudiante
+public boolean agregarEstudiante(Estudiante estudiante){
+    PreparedStatement ps;
+    Connection con = getConnection();
+    String sql = 'INSERT INTO estudiantes2022 (nombre, apellido, telefono, email) VALUES (?,?,?,?)';
+    try{
+        ps = con.PreparedStatement(sql);
+        ps.setString(1, estudiante.getNombre());
+        ps.setString(2, estudiante.getApellido());
+        ps.setString(3, estudiante.getTelefono());
+        ps.setString(4, estudiante.getEmail());
+    } catch (Exception e) {
+        System.out.println("Ocurrio un error al agregar estudiante: "+e.getMessage()); 
+    }
 
+}
