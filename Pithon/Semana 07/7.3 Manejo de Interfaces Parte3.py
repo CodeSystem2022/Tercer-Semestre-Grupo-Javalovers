502777a6bdc28f69dd545e082f6af6b708be1ea3
Javier <112019412+javierr66@users.noreply.github.com>
2023-06-30 18:18:58 -0300

    7.3 Manejo de Interfaces Parte3.py

---
+++ Pithon/Semana 07/7.3 Manejo de Interfaces Parte3.py
@@ -0,0 +1,10 @@
+import logging as log
+
+#Llamamos una configuracion básica
+
+if __name__ =='__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warnig')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
