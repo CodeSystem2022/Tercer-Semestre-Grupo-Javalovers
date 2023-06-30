10b7762547c93c2a35bab1c2bb3c8217abf45a9c
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-06-30 20:24:50 -0300

    Create 1.4 Cómo acceder a las propiedades de los Objetos.js

---
+++ JavaScript/semana 04/1.4 Cómo acceder a las propiedades de los Objetos.js
@@ -0,0 +1,12 @@
+let persona2 = new Object();//Debe crear un nuevo objeto en memoria 
+persona2.nombre = 'Juan';
+persona2.direccion = 'Salada 14';
+persona2.telefono = '5492618282821';
+console.log(persona2.telefono); '5492618282821';
+
+console.log(persona['apellido']);// accedemos como si fuera un arreglo de Gil
+//for in
+for(propidad in persona){
+    console.log(propidad); nombre, apellido, email, edad, nombreCompleto
+    console.log(persona[propidad]); Carlos, Gil, cgil@gmail.com, 30, 
+}
