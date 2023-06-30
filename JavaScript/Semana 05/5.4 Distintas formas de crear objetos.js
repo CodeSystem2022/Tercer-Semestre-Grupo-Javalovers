32d38bf6c3f770dda2200bbb4f513dd4ac0202ed
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-06-30 20:27:10 -0300

    Create 5.4 Distintas formas de crear objetos.js

---
+++ JavaScript/Semana 05/5.4 Distintas formas de crear objetos.js
@@ -0,0 +1,30 @@
+// Caso numero 1
+
+let miObjeto = new Object(); // Esto es una opcion formal
+// Caso numero 2
+let miObjeto2 = {}; // Esta opcion es breve y recomendada
+
+// Caso String 1 
+let miCadena1 = new String('Hola'); // Sintaxis formal
+// Caso String 2
+let miCadena2 = 'Hola'; // Esta es la sintaxis simplificada y recomendada
+
+// Caso con numeros 1
+let miNumero = new Number(1); // Es formal no recomendable
+// Caso con numeros 2
+let miNumero2 = 1; // Sintaxis recomendada
+
+// Caso boolean 1
+let miBoolean1 = new Boolean(false); // Formal
+// Caso boolean 2
+let miBoolean2 = false; // Sintaxis recomendada
+
+// Caso Arreglos 1
+let miArreglo1 = new Array(); // Formal
+// Caso Arreglos 2
+let miArreglo2 = []; // Sintaxis recomendada
+
+// Caso function 1
+let miFuncion1 = new function(){}; // Todo despues de new es  considerado objeto
+// Caso function 2
+let miFuncion2 = function(){}; // Notacion simplificada y recomendada
