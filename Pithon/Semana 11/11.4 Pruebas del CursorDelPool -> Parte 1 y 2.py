584499782b5da280c053528e6e5d51be9aad2b81
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-07-01 01:51:06 -0300

    Update 11.4 Pruebas del CursorDelPool -> Parte 1 y 2.py

--- Pithon/Semana 11/11.4 Pruebas del CursorDelPool -> Parte 1 y 2.py
+++ Pithon/Semana 11/11.4 Pruebas del CursorDelPool -> Parte 1 y 2.py
@@ -1,1 +1,6 @@
-
+if __name__ =='__main__':
+    with CursorDelPool() as cursor:
+        log.debug('Dentro del bloquew with')
+        cursor.execute('SELECT * FROM persona')
+        log.debug(cursor.fetchall())
+        
