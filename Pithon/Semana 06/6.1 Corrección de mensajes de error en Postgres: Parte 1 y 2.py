f2d5391252e24627d52aeb7249c2a908bc0ed7bf
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-07-01 00:45:43 -0300

    Update 6.1 Corrección de mensajes de error en Postgres: Parte 1 y 2.py

--- Pithon/Semana 06/6.1 Corrección de mensajes de error en Postgres: Parte 1 y 2.py
+++ Pithon/Semana 06/6.1 Corrección de mensajes de error en Postgres: Parte 1 y 2.py
@@ -18,4 +18,23 @@ try:
 except Exception as e:
     print(f'Ocurrió un error: {e}')
 finally: 
- 
+     conexion.close()
+    
+#VIDEO DE LA CLASE 6: video 1
+import psycopg2  #Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect( user= 'postgres1', password= 'admin', host= '127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FORM persona WHERE ide_persona = %s' #Plceholder
+            ide_persona = input('Digite un número para el ide_persona: ')
+            cursor.execute(sentencia, (ide_persona,)) #De esta manera ejecutamos la sentencia
+            registros = cursor.fetchone()#Recuperamos todos los registros que serán una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally: 
+    conexion.close()
+
+#https://www.psycopg.org/docs/usage.html
