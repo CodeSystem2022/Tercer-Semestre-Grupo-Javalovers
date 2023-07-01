93c14025f63f288f7992da487f76cb64ec438ef2
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-06-30 21:03:38 -0300

    Update 9.8 Prueba Clase Cliente.js

--- JavaScript/Semana 9/9.8 Prueba Clase Cliente.js
+++ JavaScript/Semana 9/9.8 Prueba Clase Cliente.js
@@ -1,1 +1,6 @@
+// Prueba clase Cliente
+let cliente1 = new Cliente('Miguel','Zala',29,new Date());
+console.log(cliente1.toString());
 
+let cliente2 = new Cliente('Natalia','Ortega',22,new Date());
+console.log(cliente2.toString());
