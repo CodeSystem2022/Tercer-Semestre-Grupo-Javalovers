2f800059074461a862abd20c06f2078b28e8acd6
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-07-01 00:43:11 -0300

    Update 6.2 Método Get y Set: Parte Get y Parte Set.js

--- JavaScript/Semana 06/6.2 Método Get y Set: Parte Get y Parte Set.js
+++ JavaScript/Semana 06/6.2 Método Get y Set: Parte Get y Parte Set.js
@@ -12,3 +12,14 @@ class Persona{ //Clase padre
         this._nombre = nombre;
     }
 }
+# parte 2
+let persona1 = new Persona('Martin','Perez');
+console.log(persona1.nombre);
+persona1.nombre= 'Juan Carlos';
+console.log(persona1.nombre);
+//console.log(persona1);
+let persona2 = new Persona('Carlos','Lara');
+console.log(persona2.nombre);
+persona2.nombre= 'María Laura';
+console.log(persona2.nombre);
+//console.log(persona2);
