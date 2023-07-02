f98e8348281467adba219c8604bc31156b96d333
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-07-02 02:34:58 -0300

    Create 6.2 Manejo de transacciones (Manualmente): Parte 1, 2, 3 y 4.py

---
+++ Pithon/Semana 06/6.2 Manejo de transacciones (Manualmente): Parte 1, 2, 3 y 4.py
@@ -0,0 +1,31 @@
+import psycopg2 as bd
+conexion = bd.connect(
+    user='postgres',
+    password='mysecretpassword',
+    host='localhost',
+    port='5432',
+    database='utn'
+)
+print(conexion)
+
+try:
+    conexion.autocommit = False # Evitamos el guardado de informacion de manera automatica. MALA PRACTICA
+    cursor = conexion.cursor()
+    sentencia = "INSERT INTO PERSONAS (nombre, apellido, email) VALUES (%s, %s, %s)"
+    valores = ("hernan", "suarez", "csuares@gmail.com")
+    cursor.execute(sentencia, valores)
+    print('Termina la transaccion de insert.')
+
+    sentenciaUpdate = "UPDATE personas SET nombre=%s, apellido=%s, email=%s WHERE id_personas = %s"
+    valoresUpdate = ("CARLOS", "PAEZ", "CPAEZ@mail.com", 29
+    cursor.execute(sentenciaUpdate, valoresUpdate)
+
+    print('Termina la transaccion de update.')
+
+    conexion.commit()
+
+except Exception as ex:
+    conexion.rollback()
+    print(f'Ocurrio un error {ex}')
+finally:
+    conexion.close()
