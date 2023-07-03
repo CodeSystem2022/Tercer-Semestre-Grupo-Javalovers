53e44c6f8ceaf76c1ee90f12a136837e7e3aa780
Arroganzt <112448203+Arroganzt@users.noreply.github.com>
2023-07-02 22:41:49 -0300

    Update 12.8 agregamos el método modificar estudiante .java

--- Java/Semana 12/12.8 agregamos el método modificar estudiante .java
+++ Java/Semana 12/12.8 agregamos el método modificar estudiante .java
@@ -1,83 +1,117 @@
-package UTN.dominio;
+package UTN.datos;
 
-public class Estudiante {
-    private int idEstudiante;
-    private String nombre;
-    private String apellido;
-    private String telefono;
-    private String email;
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import UTN.dominio.Estudiante;
 
-    public Estudiante(){} //Constructor vacío
+public class EstudianteDAO {
+    //Metod listar
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = new ArrayList<>();
+        //Creamos algunos objetos que son necesarios para comunicarnos con la base de datos
+        PreparedStatement ps; //Envia la sentencia a la base de datos
+        ResultSet rs; //Obtenemos el resultado de la base de datos
+        //Creamos un objeto de tipo conexion
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022 ORDER BY idestudiantes2022";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("idestudiantes2022"));//EN EL VIDEO NO ESTA LA S
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                //Falta agregarlo a la lista
+                estudiantes.add(estudiante);
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al seleccionar datos: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error al cerrar la conexion: " + e.getMessage());
+            }
+        }//Fin finally
+        return estudiantes;
+    } //Fin metodo listar
 
-    public Estudiante(int idEstudiante){ //Constructor para llave primaria
-        this.idEstudiante = idEstudiante;
+    //Metodo por id -> fin by id
+    public boolean buscarEstudiantePorId(Estudiante estudiante) {
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiante2022 WHERE idestudiantes2022=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true; //Se encontro un registro
+            }//Fin if
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al buscar estudiante: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error al cerrar la conexión: " + e.getMessage());
+            }//fin catch
+        }//fin finally
+        return false;
     }
 
-    //Constructor para insertar un nuevo estudiante
-    public Estudiante(String nombre, String apellido, String telefono, String email){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.email = email;
+    //Metodo agregar un nuevo estudiante
+    public boolean agregarEstudiante(Estudiante estudiante) {
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "INSERT INTO estudiantes2022(nombre, apellido, telefono, email) VALUES (?,?,?,?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.execute();
+            return true;
+        }catch(Exception e){
+            System.out.println("Ocurrio un Error al agregar estudiante" + e.getMessage());
+        }//Fin catch
+        finally{
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar la conexion: " + e.getMessage());
+            }//fin catch
+        }//fin finally
+        return false;
+    }//Fin metodo agregarEstudiante
+
+    public static void main(String[] args) {
+        //Listar los estudiantes
+        var estudianteDao = new EstudianteDAO();
+        System.out.println("Listado de estudiantes: ");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println); // Funcion lamda para imporimir
+        
+        //Agregar estudiante
+        var nuevoEstudiante = new Estudiante("Carlos", "Lara", "5499544223", "carlos@gmail.com");
+        var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
+        if(agregado)
+            System.out.println("Estudiante agregado: "+nuevoEstudiante);
+        else
+            System.out.println("No se ha agregado estudiante: "+nuevoEstudiante);
     }
 
-    //Constructor para modificar
-    public Estudiante(int idEstudiante, String nombre, String apellido, String telefono, String email){
-        this.idEstudiante = idEstudiante;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.email = email;
-    }
-
-    public int getIdEstudiante() {
-        return idEstudiante;
-    }
-
-    public void setIdEstudiante(int idEstudiante) {
-        this.idEstudiante = idEstudiante;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Estudiante{" +
-                "idEstudiante=" + idEstudiante +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
