9ecc3c70c24d3acffa89d7f68b775c05874bd94c
Arroganzt <112448203+Arroganzt@users.noreply.github.com>
2023-07-02 22:48:44 -0300

    Update 12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 1, 2, 3 y 4.java

--- Java/Semana 12/12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 1, 2, 3 y 4.java
+++ Java/Semana 12/12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 1, 2, 3 y 4.java
@@ -41,3 +41,64 @@ public class EstudiantesDA0 {
       
       
     }
+//parte 3 y 4
+        //Metodo por id -> find by id
+    public boolean buscarEstudiantePorId(Estudiante estudiante){
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection sql = "SELECT * FROM estudiantes2022 WHERE idestudiantes2022=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true; //se encontro un registro
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al buscar estudiante: "+e.getMessage());
+        }
+        finally{
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("ocurrio un error al cerrar conexion"+e.getMessage());
+            }//fin catch
+        }//fin finally
+        return false;
+
+    }
+
+    //Método agregar un nuevo estudiantes
+    public boolean agregarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "INSERT INTO estudiantes2022 (nombre, apellido, telefono, email) VALUES (?, ?, ?, ?)";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+
+        } catch(Exception e){
+            System.out.println("Ocurrio un error al agregar estudiante: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //Buscar por id
+        var estudiante1 = new Estudiante(1);
+        System.out.println("Estudiantes antes de la busqueda"+estudiante1);
+        var encontrado = estudianteDao.buscarEstudiantePorId(estudiante1);
+        if(encontrado){
+            System.out.println("Estudiante encontrado"+ estudiante1);
+        }else{
+            System.out.println("No se encontro el estudiante: "+ estudiante1.getIdEstudiante());
+        }
+    }
+}
